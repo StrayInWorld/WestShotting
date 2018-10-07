dc3b256852a91807c3e9cb796a3da99355b91e35
strayx <2232776923@qq.com>
2018-10-07 17:21:17 +0800

    添加动画

--- WestShotting/assets/Script/bg-node.js
+++ WestShotting/assets/Script/bg-node.js
@@ -11,50 +11,89 @@ cc.Class({
         let canvasNode = cc.find("Canvas");
 
         //翻转第二张背景的山的图
-        let secondBgGround2 = cc.find("groundNode1/backGround", this.node);
+        let secondBgGround2 = cc.find("backGroundNode/backGround2", this.node);
         secondBgGround2.runAction(cc.flipX(true));
 
         //翻转第二张山的图
-        let mountain2 = cc.find("groundNode1/mountain", this.node);
+        let mountain2 = cc.find("mountainNode/mountain2", this.node);
         mountain2.runAction(cc.flipX(true));
 
+        //翻转第二张前景的图
+        let frontGround2 = cc.find("frontGroundNode/frontGround2", this.node);
+        frontGround2.runAction(cc.flipX(true));
+
         //云动画
-        let groundNodeCloud = cc.find("groundNode/cloudNode", this.node);
-        let cloud2 = cc.find("groundNode/cloudNode/cloud2", this.node);
+        let groundNodeCloud = cc.find("cloudNode", this.node);
+        let cloud2 = cc.find("cloudNode/cloud2", this.node);
         let cloudOriginNodePos = groundNodeCloud.position;
         let cloudMoveX = cloud2.position.x;
-        let cloudMoveTime = 10;
+        let cloudMoveTime = 100;
         groundNodeCloud.runAction(cc.repeatForever(
             cc.sequence(
-                cc.moveBy(cloudMoveTime,cc.v2(-cloudMoveX,0)),
+                cc.moveBy(cloudMoveTime, cc.v2(-cloudMoveX, 0)),
                 cc.place(cloudOriginNodePos)
             )
         ));
 
         //前景动画
-        let frontGround = cc.find("groundNode/frontGround",this.node);
-        let secondFrontGround = cc.find("groundNode1/frontGround",this.node);
-        let resetPos = secondFrontGround.position;
-        let frontGroundMoveX = frontGround.width;
-        let secondFrontGroundMoveX = frontGroundMoveX*2;
-        let frontActionTime = 10;
+        let frontGroundNode = cc.find("frontGroundNode", this.node);
+        let frontGround = cc.find("frontGroundNode/frontGround1", this.node);
+        let secondFrontGround = cc.find("frontGroundNode/frontGround2", this.node);
+        let resetPos = cc.v2(canvasNode.width,frontGround.y);
+        let firstMoveX = frontGround.width - canvasNode.width;
+        let secondMoveX = canvasNode.width;
+        let firstActionTime = 100;
+        let secondActionTime = 30;
+
         frontGround.runAction(cc.repeatForever(
             cc.sequence(
-                cc.moveBy(cloudMoveTime,cc.v2(-frontGroundMoveX,0)),
-                cc.place(resetPos)
+                cc.moveBy(firstActionTime, cc.v2(-firstMoveX, 0)),
+                cc.callFunc(function () {
+                    cc.log("1");
+                    secondFrontGround.runAction(
+                        cc.sequence(
+                            cc.moveBy(secondActionTime, cc.v2(-secondMoveX, 0)),
+                            cc.callFunc(function () {
+                                cc.log("2");
+                            }),
+                            cc.place(resetPos)
+                        )
+                    )
+                }),
+                cc.moveBy(secondActionTime, cc.v2(-secondMoveX, 0)),
+                cc.place(resetPos),
+                // cc.delayTime(secondActionTime)
             )
         ));
 
-
-
-
-
-        let cloudAction = cc.sequence();
+        //山动画
+        let mountain = cc.find("mountainNode/mountain1", this.node);
+        let secondMountain = cc.find("mountainNode/mountain2", this.node);
+        let mountaisMovsX = mountain.width;
+        let mountainActionTime = 100;
+        mountain.runAction(cc.repeatForever(
+            cc.sequence(
+                cc.moveBy(mountainActionTime,cc.v2(-mountaisMovsX,0)),
+                cc.callFunc(function () {
+                    cc.log("1");
+                    secondMountain.runAction(
+                        cc.sequence(
+                            cc.moveBy(mountainActionTime, cc.v2(-mountaisMovsX, 0)),
+                            cc.callFunc(function () {
+                                cc.log("2");
+                            }),
+                            cc.place(resetPos)
+                        )
+                    )
+                }),
+                cc.place(resetPos)
+            )
+        ));
     },
 
     start() {
 
     },
 
-    // update (dt) {},
+    update (dt) {},
 });
--- WestShotting/assets/Script/player.js
+++ WestShotting/assets/Script/player.js
@@ -8,12 +8,6 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        // cc.director.setDisplayStats(false); 
-
-        let anima = this.getComponent(cc.Animation);
-        let animaState = anima.play();
-        cc.log(animaState.wrapMode);
-
         let leftPos = -this.node.parent.width/2;
         this.node.runAction(cc.moveTo(2,cc.v2(leftPos,this.node.y)));
     },
