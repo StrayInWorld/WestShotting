1179d8ca21b58a1b2e1d32c3f6e07a2386c668de
strayx <2232776923@qq.com>
2018-10-07 13:30:31 +0800

    添加背景动画

--- WestShotting/assets/Script/bg-node.js
+++ WestShotting/assets/Script/bg-node.js
@@ -7,13 +7,52 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
-        //翻转第二张背景的图
-        let secondBgGround2 = cc.find("groundNode1/backGround",this.node);
+    onLoad() {
+        let canvasNode = cc.find("Canvas");
+
+        //翻转第二张背景的山的图
+        let secondBgGround2 = cc.find("groundNode1/backGround", this.node);
         secondBgGround2.runAction(cc.flipX(true));
+
+        //翻转第二张山的图
+        let mountain2 = cc.find("groundNode1/mountain", this.node);
+        mountain2.runAction(cc.flipX(true));
+
+        //云动画
+        let groundNodeCloud = cc.find("groundNode/cloudNode", this.node);
+        let cloud2 = cc.find("groundNode/cloudNode/cloud2", this.node);
+        let cloudOriginNodePos = groundNodeCloud.position;
+        let cloudMoveX = cloud2.position.x;
+        let cloudMoveTime = 10;
+        groundNodeCloud.runAction(cc.repeatForever(
+            cc.sequence(
+                cc.moveBy(cloudMoveTime,cc.v2(-cloudMoveX,0)),
+                cc.place(cloudOriginNodePos)
+            )
+        ));
+
+        //前景动画
+        let frontGround = cc.find("groundNode/frontGround",this.node);
+        let secondFrontGround = cc.find("groundNode1/frontGround",this.node);
+        let resetPos = secondFrontGround.position;
+        let frontGroundMoveX = frontGround.width;
+        let secondFrontGroundMoveX = frontGroundMoveX*2;
+        let frontActionTime = 10;
+        frontGround.runAction(cc.repeatForever(
+            cc.sequence(
+                cc.moveBy(cloudMoveTime,cc.v2(-frontGroundMoveX,0)),
+                cc.place(resetPos)
+            )
+        ));
+
+
+
+
+
+        let cloudAction = cc.sequence();
     },
 
-    start () {
+    start() {
 
     },
 
