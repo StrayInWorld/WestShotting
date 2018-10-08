538a07220af01a993d30c66bf76e5d9b64161957
strayx <2232776923@qq.com>
2018-10-08 11:56:12 +0800

    添加物理边界

---
+++ WestShotting/assets/Script/PhysicsBound.js
@@ -0,0 +1,50 @@
+//创建物理边界
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        size: cc.size(0, 0)
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        let physicsManager = cc.director.getPhysicsManager();
+        physicsManager.enabled = true;
+
+        //是否绘制边界
+        physicsManager.debugDrawFlags =
+            // 0;
+            // cc.PhysicsManager.DrawBits.e_aabbBit |
+            cc.PhysicsManager.DrawBits.e_jointBit |
+            cc.PhysicsManager.DrawBits.e_shapeBit
+            ;
+
+        this.width = this.size.width || this.node.width;
+        this.height = this.size.height || this.node.height;
+
+        let node = new cc.Node();
+        let body = node.addComponent(cc.RigidBody);
+        body.type = cc.RigidBodyType.Static;
+
+        this._addBound(node, 0, -this.height / 2, this.width, 20);   //下面
+        this._addBound(node, 0, this.height / 2, this.width, 20);   //上面
+        this._addBound(node, -this.width / 2, 0, 20, this.height);  //左边
+        this._addBound(node, this.width / 2, 0, 20, this.height);   //右边
+
+        let collider = node.getComponent(cc.Collider);
+        collider.apply();
+        node.parent = this.node;
+    },
+
+    _addBound(node, x, y, width, height) {
+        let collider = node.addComponent(cc.PhysicsBoxCollider);
+        collider.offset.x = x;
+        collider.offset.y = y;
+        collider.size.width = width;
+        collider.size.height = height;
+    },
+
+    createOtherSideBound() {
+
+    },
+});
---
+++ WestShotting/assets/Script/game.js
@@ -0,0 +1,31 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {      
+        // //leftTop  
+        // let tri2 = cc.find("Canvas/triangle_2");
+        // tri2.runAction(cc.flipX(true));
+        
+        // //rightBottom
+        // let tri3 = cc.find("Canvas/triangle_3");
+        // tri3.runAction(cc.flipY(true));
+
+        // //leftBottom
+        // let tri4 = cc.find("Canvas/triangle_4");
+        // tri4.runAction(cc.sequence(cc.flipX(true),cc.flipY(true)));
+
+
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
