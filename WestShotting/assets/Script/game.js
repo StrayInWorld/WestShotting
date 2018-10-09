53e70e3451e5360b0f4e0d1e97563a708e4a10dd
strayx <2232776923@qq.com>
2018-10-09 16:37:03 +0800

    增加屏幕移动时的枪移动

--- WestShotting/assets/Script/game.js
+++ WestShotting/assets/Script/game.js
@@ -2,31 +2,15 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-       doubleChain:cc.Node
+        doubleChain: cc.Node
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {      
-        // //leftTop  
-        // let tri2 = cc.find("Canvas/Triangle2");
-        // tri2.runAction(cc.flipX(true));
-        
-        // //rightBottom
-        // let tri3 = cc.find("Canvas/Triangle3");
-        // tri3.runAction(cc.flipY(true));
-
-        // //leftBottom
-        // let tri4 = cc.find("Canvas/Triangle4");
-        // tri4.runAction(cc.sequence(cc.flipX(true),cc.flipY(true)));
-
-
-
-
-
+    onLoad() {
     },
 
-    start () {
+    start() {
 
     },
 
